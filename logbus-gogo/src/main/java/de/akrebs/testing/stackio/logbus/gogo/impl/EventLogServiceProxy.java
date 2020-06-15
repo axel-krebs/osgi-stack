1516258ad988fb607cd61f6ed95035533a1a8848
axel-krebs <axel.krebs@online.de>
2020-06-15 09:58:11 +0200

    Alignment to OSGi standards

--- logbus-gogo/src/main/java/de/akrebs/testing/stackio/logbus/gogo/impl/EventLogServiceProxy.java
+++ logbus-gogo/src/main/java/de/akrebs/testing/stackio/logbus/gogo/impl/EventLogServiceProxy.java
@@ -1,51 +1,54 @@
 package de.akrebs.testing.stackio.logbus.gogo.impl;
 
 import org.apache.felix.service.command.Descriptor;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+import de.akrebs.testing.stackio.logbus.api.EventLogger;
 
 @Component(
-	property = { 
-		"osgi.command.scope=membus", 
-		"osgi.command.function=log_message" 
-	},
-	service=EventLogServiceProxy.class
-)
+	property = 
+    		{ "osgi.command.scope=logbus",
+    		  "osgi.command.function=log_message" 
+    		}, 
+    	service = EventLogServiceProxy.class)
 public class EventLogServiceProxy {
-	
-	//private BundleContext context;
 
-//	public EventLogServiceProxy(BundleContext context) {
-//		super();
-//		this.context = context;
-//	}
-	
-	@Descriptor("Log a message.")
-	public void log_message(@Descriptor("The message that should be logged") String message) {
-		
-//		EventLogger logger = lookupService();
-//		logger.log(message);
-	    
-	    System.out.println("Hello, " + message);
+    @Reference
+    private EventLogger loggingService;
+
+    private volatile boolean active = false;
+
+    /**
+     * Activate logging as soon as service becomes available.
+     * 
+     * @param context
+     */
+    @Activate
+    void activate(ComponentContext context) {
+	active = true;
+    }
+
+    @Deactivate
+    void deactivate(ComponentContext context) {
+	active = false;
+    }
+
+    @Descriptor("Log a message.")
+    public void log_message(@Descriptor("The message that should be logged") String message) {
+
+	if (active) {
+
+	    loggingService.log(message);
 	}
 	
-//	protected EventLogger lookupService() {
-//		
-//		ServiceReference<EventLogger> ref = context.getServiceReference(EventLogger.class);
-//		
-//		if(ref == null) {
-//			
-//			throw new RuntimeException("Cannot get service reference to EventLogger.");
-//		}
-//		
-//		EventLogger logger = context.getService(ref);
-//		
-//		if(logger == null) {
-//			
-//			throw new RuntimeException("No implementations found for EventLogger!");
-//		}
-//		
-//		return logger;
-//		
-//	}
+	else {
+	    
+	    System.out.println("Was not active!");
+	}
+    }
 
 }
