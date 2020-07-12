3f1771e9a513220d5af1b000141a3bffb6ae7274
axel-krebs <axel.krebs@online.de>
2020-07-12 12:29:55 +0200

    changed to bnd-maven-plugin instead of maven-bundle-plugin

--- stack-util-sun-misc/src/test/java/de/akrebs/stackio/util/sun/misc/LoadUnsafeTest.java
+++ stack-util-sun-misc/src/test/java/de/akrebs/stackio/util/sun/misc/LoadUnsafeTest.java
@@ -1,10 +1,12 @@
 package de.akrebs.stackio.util.sun.misc;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class LoadUnsafeTest extends TestCase {
 
-    //@Test
+    @Test
     public void test() throws ClassNotFoundException {
 	Class<?> unsafe = Class.forName("com.sun.misc.Unsafe");
 	assertNotNull(unsafe);
