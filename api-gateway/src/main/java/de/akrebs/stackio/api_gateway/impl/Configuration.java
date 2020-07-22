d31c14113206afe80d34c6ea6460a1e8c60e5a13
axel-krebs <axel.krebs@online.de>
2020-07-22 15:03:08 +0200

    deleted

--- api-gateway/src/main/java/de/akrebs/stackio/api_gateway/impl/Configuration.java
+++ api-gateway/src/main/java/de/akrebs/stackio/api_gateway/impl/Configuration.java
@@ -1,18 +1,47 @@
 package de.akrebs.stackio.api_gateway.impl;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import javax.servlet.ServletException;
+
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.http.HttpContext;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+import org.osgi.service.log.LogService;
 
 @Component(immediate = true, service = Configuration.class)
 public class Configuration {
 
     volatile boolean standalone = true;
 
+    @Reference
+    HttpService httpService;
+
+    @Reference
+    LogService logger;
+
     @Activate
     public void start() {
 
 	System.out.println("Starting configuration..");
+
+	HttpContext httpContext = createHttpContextFromConfig(httpService);
+
+	try {
+	    addStaticResources(httpService, httpContext);
+	    addDynamicMapping(httpService, httpContext);
+	} catch (NamespaceException e) {
+	    // TODO log
+	    e.printStackTrace();
+	} catch (ServletException e) {
+	    // TODO log
+	    e.printStackTrace();
+	}
 
     }
 
@@ -20,6 +49,39 @@ public class Configuration {
     public void stop() {
 
 	System.out.println("Stopping configuration..");
+
+	removeStaticResources();
+
+	removeDynamicMapping();
+    }
+
+    private void removeStaticResources() {
+	httpService.unregister("/");
+    }
+
+    private void addStaticResources(HttpService httpService, HttpContext httpContext) throws NamespaceException {
+	httpService.registerResources("/", "static/index.html", httpContext);
+    }
+
+    private void addDynamicMapping(HttpService httpService, HttpContext httpContext)
+	    throws ServletException, NamespaceException {
+
+	Dictionary<String, String> initParams = new Hashtable<String, String>();
+	httpService.registerServlet("/eins", new ResourceServlet("eins"), initParams, httpContext);
+	httpService.registerServlet("/zwei", new ResourceServlet("zwei"), initParams, httpContext);
+    }
+
+    private void removeDynamicMapping() {
+	httpService.unregister("/eins");
+	httpService.unregister("/zwei");
+    }
+
+    /*
+     * TODO: This context has no security yet!
+     */
+    private HttpContext createHttpContextFromConfig(HttpService httpService) {
+	HttpContext context = httpService.createDefaultHttpContext();
+	return context;
     }
 
 }
--- api-gateway/src/main/java/de/akrebs/stackio/api_gateway/osgi/Activator.java
+++ api-gateway/src/main/java/de/akrebs/stackio/api_gateway/osgi/Activator.java
@@ -1,19 +1,8 @@
 package de.akrebs.stackio.api_gateway.osgi;
 
-import java.util.Dictionary;
-import java.util.Hashtable;
-
-import javax.servlet.ServletException;
-
 import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.http.HttpContext;
-import org.osgi.service.http.HttpService;
-import org.osgi.service.http.NamespaceException;
-
-import de.akrebs.stackio.api_gateway.impl.ResourceServlet;
 
 /**
  * Will be executed each time the bundle starts; to be replaced by annotations!
@@ -25,39 +14,7 @@ public class Activator extends DependencyActivatorBase {
     @Override
     public void init(BundleContext context, DependencyManager manager) throws Exception {
 
-	ServiceReference<HttpService> ref = context.getServiceReference(HttpService.class);
-	HttpService httpService = context.getService(ref);
-	HttpContext httpContext = createHttpContextFromConfig(httpService);
-
-	if (null != httpService) {
-	    addStaticResources(httpService, httpContext);
-
-	    // Instantiate Jersey servlet with given config
-	    addDynamicMapping(httpService, httpContext);
-	} else {
-
-	    // TODO log
-	}
-    }
-
-    private void addStaticResources(HttpService httpService, HttpContext httpContext) throws NamespaceException {
-	httpService.registerResources("/", "static/index.html", httpContext);
-    }
-
-    private void addDynamicMapping(HttpService httpService, HttpContext httpContext)
-	    throws ServletException, NamespaceException {
-
-	Dictionary<String, String> initParams = new Hashtable<String, String>();
-	httpService.registerServlet("/eins", new ResourceServlet("eins"), initParams, httpContext);
-	httpService.registerServlet("/zwei", new ResourceServlet("zwei"), initParams, httpContext);
-    }
-
-    /*
-     * TODO: This context has no security yet!
-     */
-    private HttpContext createHttpContextFromConfig(HttpService httpService) {
-	HttpContext context = httpService.createDefaultHttpContext();
-	return context;
+	System.out.println("API-GATEWAY: Activator.init()s");
     }
 
 }
